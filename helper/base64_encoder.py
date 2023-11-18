c7b1f49a998aef17be70b66c05b2ba3751a892bd
Serkan Haytac <serkanhaytac@IHM-SAT-SHAYTAC2.local>
2023-11-18 09:57:56 -0500

    updated

---
+++ helper/base64_encoder.py
@@ -0,0 +1,6 @@
+import base64
+
+def convert_image_to_base64(image_path):
+    with open(image_path, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read()).decode()
+    return encoded_string
